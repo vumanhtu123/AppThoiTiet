be357d8f08b9f0973ede3090ee04cdeec8f053c1
vumanhtu123 <vutu1706@gmail.com>
2022-07-15 00:45:39 +0700

    layout weather

---
+++ app/src/main/java/com/example/appthoitiet/fragment/FragmentMainHome.java
@@ -0,0 +1,21 @@
+package com.example.appthoitiet.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.appthoitiet.R;
+
+public class FragmentMainHome extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return view = inflater.inflate(R.layout.activity_fragment_main_home,container,false);
+    }
+}
